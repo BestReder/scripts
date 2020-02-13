00a36b55e48dbee04b05f30f21e6a3f088256c1f
BestReder <997594560@qq.com>
2020-02-13 19:55:00 +0800

    updtae

--- burp.sh
+++ burp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd ~/tools/抓包工具/burp && java -Xbootclasspath/p:burp-loader-keygen.jar -jar burpsuite_pro_v2.0beta.jar
+cd ~/tools/burp && java -Xbootclasspath/p:burp-loader-keygen.jar -jar burpsuite_pro_v2.0beta.jar
