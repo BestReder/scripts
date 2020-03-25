4d5af09cabee08278a760b8942d97370408d0a19
BestReder <997594560@qq.com>
2020-03-25 10:35:15 +0800

    updata

--- vol-toggle.sh
+++ vol-toggle.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/bin/amixer set Master toggle
-bash ~/github/scripts/dwm-status-refresh.sh
+amixer set Master toggle
+
--- vol-up.sh
+++ vol-up.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-#/usr/bin/amixer -qM set Master 5%+ umute
-pactl set-sink-volume @DEFAULT_SINK@ +5%
-bash ~/github/scripts/dwm-status-refresh.sh
+amixer -qM set Master 4%+ umute
+
