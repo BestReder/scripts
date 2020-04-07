d79e2e6e92c17adc82dc42b40fb3c709bcc26fc1
BestReder <997594560@qq.com>
2020-04-07 17:31:58 +0800

    updata

--- autostart.sh
+++ autostart.sh
@@ -3,11 +3,13 @@
 xrandr --auto --output HDMI1 --same-as eDP1 --size 1920x1080
 /bin/bash ~/github/scripts/dwm-status.sh &
 /bin/bash ~/github/scripts/wp-autochange.sh &
-picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
+#picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
+picom -b
 /bin/bash ~/github/scripts/touchpad.sh &
 nm-applet &
 fcitx &
 blueman-applet &
 xfce4-power-manager &
+/bin/bash ~/scripts/run-mailsync.sh &
 #nohup /usr/lib/gsd-xsettings > /dev/null 2>&1 &
 #xautolock -time 5 -locker i3lock-fancy &
